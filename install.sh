c9748ee8ed5cb2f1cf257dcb2792039feec993fc
Farhan <farhanferoz.dhanani@gmail.com>
2024-08-05 20:21:42 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,21 @@ cd training/ || { echo "Failed to cd into training directory"; exit 1; }
 python -m venv venv || { echo "Failed to create virtual environment"; exit 
 1; }
 
+# Activate the virtual environment
+source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+
+# Activate install pre-required dependencies
+pip install ipykernel
+pip install torch xformers
+
+# Deactivate virtual enviroment
+deactivate
+
 # Modify the venv configuration file to include system site packages
 sed -i 's/^include-system-site-packages = false$/include-system-site-packages = true/' venv/pyvenv.cfg || { echo "Failed to modify venv configuration"; exit 1; }
 
-
-# Activate the virtual environment
-source venv/bin/activate || { echo "Failed to activate virtual 
-environment"; exit 1; }
+# Again Activate the virtual environment
+source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
 
 # Install required Python packages
 pip install torch==2.1.0+cu118 xformers accelerate diffusers transformers comet_ml peft bitsandbytes gdown || { echo "Failed to install Python packages"; exit 1; }
